b60243c50b6d40e8dfcc48e62672eda1e8d704fc
Estiven312 <estivencarva123@gmail.com>
2023-10-25 15:58:33 -0500

    commit

--- resources/views/compras.blade.php
+++ resources/views/compras.blade.php
@@ -5,208 +5,264 @@
 @endsection
 
 @section('content')
-<div class="container-fluid">
-    <div class="row">
+    <div class="container-fluid">
+        <div class="row">
 
-        <div class="col-12 primera_seccion">
-            <div class="row">
-                <div class="col-12 espacio">
+            <div class="col-12 primera_seccion">
+                <div class="row">
+                    <div class="col-12 espacio">
 
-                </div>
-
-                <div class="col-12">
-
-                    <div class="row">
-                        <div class="col-md-6 col-sm-12 titulo">
-                            <h1>GUIA DE COMPRA</h1>
-                        </div>
-                        <div class="col-md-6 col-sm-12 iconos">
-                            <div class="contenedor">
-
-
-                                <div class="contenedor_iconos" style=" background-color:#f7e5c7 ;">
-                                    <a href=""> <img src="{{ asset('/img/cuchillo.png') }}" alt="cuchillo"></a>
-
-                                </div>
-                                <div class="contenedor_iconos">
-                                    <a href=""><img src="{{ asset('/img/sarten.png') }}" alt="cuchillo"></a>
-
-                                </div>
-                                <div class="contenedor_iconos">
-                                    <a href=""><img src="{{ asset('/img/electrodomestico.png') }}"
-                                            alt="cuchillo"></a>
-
-                                </div>
-                            </div>
-
-                        </div>
                     </div>
 
+                    <div class="col-12">
+
+                        <div class="row">
+                            <div class="col-md-12 col-sm-12 titulo">
+                                <div class="contenedor_borde">
+                                    <h1>GUÍAS DE COMPRA</h1>
+                                </div>
+                            </div>
+                            <div class="col-md-12 col-sm-12 iconos">
+                                <div class="contenedor ">
+
+
+                                    <div class="contenedor_iconos " style=" ">
+                                        <a href="">
+                                            <div class="row">
+                                                <div class="col-12">
+                                                    <img src="{{ asset('/img/plato.png') }}" alt="cuchillo">
+
+                                                </div>
+                                                <div class="col-12">
+                                                    <p>Loza</p>
+
+                                                </div>
+                                            </div>
+                                        </a>
+
+
+
+                                    </div>
+                                    <div class="contenedor_iconos ">
+
+                                        <a href="">
+                                            <div class="row">
+                                                <div class="col-12">
+                                                    <img src="{{ asset('/img/cuchillo.png') }}" alt="cuchillo">
+
+                                                </div>
+                                                <div class="col-12">
+                                                    <p>Cuchillos</p>
+
+                                                </div>
+                                            </div>
+
+                                        </a>
+                                    </div>
+                                    <div class="contenedor_iconos">
+                                        <a href="">
+
+                                            <div class="row">
+                                                <div class="col-12">
+                                                    <img src="{{ asset('/img/sarten.png') }}" alt="cuchillo">
+
+                                                </div>
+                                                <div class="col-12">
+                                                    <p>Sartenes</p>
+
+                                                </div>
+                                            </div>
+
+                                        </a>
+                                    </div>
+                                    <div class="contenedor_iconos ">
+
+                                        <a href="">
+                                            <div class="row">
+                                                <div class="col-12">
+                                                    <img src="{{ asset('/img/electrodomestico.png') }}" alt="cuchillo">
+                                                   
+                                                </div>
+                                                <div class="col-12">
+                                                    <p>Electrodomesticos</p>
+
+
+                                                </div>
+                                            </div>
+
+                                        </a>
+                                    </div>
+
+                                </div>
+
+                            </div>
+                        </div>
 
 
 
 
+
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div class="col-12 ">
-
-
-            <div class="row">
+            <div class="col-12 ">
 
 
+                <div class="row">
 
-                <div class="col-md-9 col-sm-12 contenedor_guias">
-                    <div class="row">
 
-                        <div class="col-md-4 contenedor_guia  col-sm-12">
-                            <div class="guia">
-                                <a href="">
-                                    <div class="contenedor_img">
-                                        <img src="{{ asset('/img/knives.webp') }}" alt="cuchillo">
 
-                                    </div>
-                                    <div class="contenedor_descripcion">
-                                        <span>Categoria</span>
-                                        <h3>Nombre de la guia</h3>
+                    <div class="col-md-9 col-sm-12 contenedor_guias">
+                        <div class="row">
 
-                                    </div>
-                                </a>
+                            <div class="col-md-3 contenedor_guia  col-sm-12">
+                                <div class="guia">
+                                    <a href="">
+                                        <div class="contenedor_img">
+                                            <img src="{{ asset('/img/knives.webp') }}" alt="cuchillo">
+
+                                        </div>
+                                        <div class="contenedor_descripcion">
+                                            <span>Categoria</span>
+                                            <h3>Nombre de la guia</h3>
+
+                                        </div>
+                                    </a>
+                                </div>
                             </div>
-                        </div>
-                        <div class="col-md-4 contenedor_guia  col-sm-12">
-                            <div class="guia">
-                                <a href="">
-                                    <div class="contenedor_img">
-                                        <img src="{{ asset('/img/knives.webp') }}" alt="cuchillo">
+                            <div class="col-md-3 contenedor_guia  col-sm-12">
+                                <div class="guia">
+                                    <a href="">
+                                        <div class="contenedor_img">
+                                            <img src="{{ asset('/img/knives.webp') }}" alt="cuchillo">
 
-                                    </div>
-                                    <div class="contenedor_descripcion">
-                                        <span>Categoria</span>
-                                        <h3>Nombre de la guia</h3>
+                                        </div>
+                                        <div class="contenedor_descripcion">
+                                            <span>Categoria</span>
+                                            <h3>Nombre de la guia</h3>
 
-                                    </div>
-                                </a>
+                                        </div>
+                                    </a>
+                                </div>
                             </div>
-                        </div>
-                        <div class="col-md-4 contenedor_guia  col-sm-12">
-                            <div class="guia">
-                                <a href="">
-                                    <div class="contenedor_img">
-                                        <img src="{{ asset('/img/knives.webp') }}" alt="cuchillo">
+                            <div class="col-md-3 contenedor_guia  col-sm-12">
+                                <div class="guia">
+                                    <a href="">
+                                        <div class="contenedor_img">
+                                            <img src="{{ asset('/img/knives.webp') }}" alt="cuchillo">
 
-                                    </div>
-                                    <div class="contenedor_descripcion">
-                                        <span>Categoria</span>
-                                        <h3>Nombre de la guia</h3>
+                                        </div>
+                                        <div class="contenedor_descripcion">
+                                            <span>Categoria</span>
+                                            <h3>Nombre de la guia</h3>
 
-                                    </div>
-                                </a>
+                                        </div>
+                                    </a>
+                                </div>
                             </div>
-                        </div>
-                        <div class="col-md-4 contenedor_guia  col-sm-12">
-                            <div class="guia">
-                                <a href="">
-                                    <div class="contenedor_img">
-                                        <img src="{{ asset('/img/knives.webp') }}" alt="cuchillo">
+                            <div class="col-md-3 contenedor_guia  col-sm-12">
+                                <div class="guia">
+                                    <a href="">
+                                        <div class="contenedor_img">
+                                            <img src="{{ asset('/img/knives.webp') }}" alt="cuchillo">
 
-                                    </div>
-                                    <div class="contenedor_descripcion">
-                                        <span>Categoria</span>
-                                        <h3>Nombre de la guia</h3>
+                                        </div>
+                                        <div class="contenedor_descripcion">
+                                            <span>Categoria</span>
+                                            <h3>Nombre de la guia</h3>
 
-                                    </div>
-                                </a>
+                                        </div>
+                                    </a>
+                                </div>
                             </div>
-                        </div>
-                        <div class="col-md-4 contenedor_guia  col-sm-12">
-                            <div class="guia">
-                                <a href="">
-                                    <div class="contenedor_img">
-                                        <img src="{{ asset('/img/knives.webp') }}" alt="cuchillo">
+                            <div class="col-md-3 contenedor_guia  col-sm-12">
+                                <div class="guia">
+                                    <a href="">
+                                        <div class="contenedor_img">
+                                            <img src="{{ asset('/img/knives.webp') }}" alt="cuchillo">
 
-                                    </div>
-                                    <div class="contenedor_descripcion">
-                                        <span>Categoria</span>
-                                        <h3>Nombre de la guia</h3>
+                                        </div>
+                                        <div class="contenedor_descripcion">
+                                            <span>Categoria</span>
+                                            <h3>Nombre de la guia</h3>
 
-                                    </div>
-                                </a>
+                                        </div>
+                                    </a>
+                                </div>
                             </div>
-                        </div>
-                        <div class="col-md-4 contenedor_guia  col-sm-12">
-                            <div class="guia">
-                                <a href="">
-                                    <div class="contenedor_img">
-                                        <img src="{{ asset('/img/knives.webp') }}" alt="cuchillo">
+                            <div class="col-md-3 contenedor_guia  col-sm-12">
+                                <div class="guia">
+                                    <a href="">
+                                        <div class="contenedor_img">
+                                            <img src="{{ asset('/img/knives.webp') }}" alt="cuchillo">
 
-                                    </div>
-                                    <div class="contenedor_descripcion">
-                                        <span>Categoria</span>
-                                        <h3>Nombre de la guia</h3>
+                                        </div>
+                                        <div class="contenedor_descripcion">
+                                            <span>Categoria</span>
+                                            <h3>Nombre de la guia</h3>
 
-                                    </div>
-                                </a>
+                                        </div>
+                                    </a>
+                                </div>
                             </div>
-                        </div>
-                        <div class="col-md-4 contenedor_guia  col-sm-12">
-                            <div class="guia">
-                                <a href="">
-                                    <div class="contenedor_img">
-                                        <img src="{{ asset('/img/knives.webp') }}" alt="cuchillo">
+                            <div class="col-md-3 contenedor_guia  col-sm-12">
+                                <div class="guia">
+                                    <a href="">
+                                        <div class="contenedor_img">
+                                            <img src="{{ asset('/img/knives.webp') }}" alt="cuchillo">
 
-                                    </div>
-                                    <div class="contenedor_descripcion">
-                                        <span>Categoria</span>
-                                        <h3>Nombre de la guia</h3>
+                                        </div>
+                                        <div class="contenedor_descripcion">
+                                            <span>Categoria</span>
+                                            <h3>Nombre de la guia</h3>
 
-                                    </div>
-                                </a>
+                                        </div>
+                                    </a>
+                                </div>
                             </div>
-                        </div>
-                        <div class="col-md-4 contenedor_guia  col-sm-12">
-                            <div class="guia">
-                                <a href="">
-                                    <div class="contenedor_img">
-                                        <img src="{{ asset('/img/knives.webp') }}" alt="cuchillo">
+                            <div class="col-md-3 contenedor_guia  col-sm-12">
+                                <div class="guia">
+                                    <a href="">
+                                        <div class="contenedor_img">
+                                            <img src="{{ asset('/img/knives.webp') }}" alt="cuchillo">
 
-                                    </div>
-                                    <div class="contenedor_descripcion">
-                                        <span>Categoria</span>
-                                        <h3>Nombre de la guia</h3>
+                                        </div>
+                                        <div class="contenedor_descripcion">
+                                            <span>Categoria</span>
+                                            <h3>Nombre de la guia</h3>
 
-                                    </div>
-                                </a>
+                                        </div>
+                                    </a>
+                                </div>
                             </div>
-                        </div>
-                        <div class="col-md-4 contenedor_guia  col-sm-12">
-                            <div class="guia">
-                                <a href="">
-                                    <div class="contenedor_img">
-                                        <img src="{{ asset('/img/knives.webp') }}" alt="cuchillo">
+                            <div class="col-md-3 contenedor_guia  col-sm-12">
+                                <div class="guia">
+                                    <a href="">
+                                        <div class="contenedor_img">
+                                            <img src="{{ asset('/img/knives.webp') }}" alt="cuchillo">
 
-                                    </div>
-                                    <div class="contenedor_descripcion">
-                                        <span>Categoria</span>
-                                        <h3>Nombre de la guia</h3>
+                                        </div>
+                                        <div class="contenedor_descripcion">
+                                            <span>Categoria</span>
+                                            <h3>Nombre de la guia</h3>
 
-                                    </div>
-                                </a>
+                                        </div>
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
 
-                
-                <div class="col-md-3 col-sm-12 contenedor-anuncio">
 
-                  <div class="anuncio">
+                    <div class="col-md-3 col-sm-12 contenedor-anuncio">
 
-                  </div>
+                        <div class="anuncio">
+
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
